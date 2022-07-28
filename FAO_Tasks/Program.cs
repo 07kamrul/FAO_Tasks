da5e9a25fdf33e25189476595383c0f112878f49
07kamrul <mdkamrulhasanewu@gmail.com>
2022-07-29 04:10:06 +0600

    Ffrist Indicators Successfully Done

--- FAO_Tasks/Program.cs
+++ FAO_Tasks/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FAO_Tasks.Services;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -11,67 +12,32 @@ namespace FAO_Tasks
     {
         static void Main(string[] args)
         {
-            /*string path1 = @"D:\Practice\ASA\FAO_Tasks\FAO_Tasks\data\data_cases_1.csv";
-            string path2 = @"D:\Practice\ASA\FAO_Tasks\FAO_Tasks\data\data_cases_2.csv";
-*/
-            string path1 = @"F:\Interview\FAO_Tasks\data\data_cases_1.csv";
-            string path2 = @"F:\Interview\FAO_Tasks\data\data_cases_2.csv";
+            Console.WriteLine("Hello FAO:");
+            Console.WriteLine("Press 1 First Indicators");
+            Console.WriteLine("Press 2 Advanced Indicators");
+            Console.WriteLine("Press 3 Corrupted Indicators");
 
-            string[] paths = new string[] { path1, path2 };
+            string path1 = @"F:\Interview\FAO_Tasks\InputData\data_cases_1.csv";
 
-            List <DataCases> dataCases = new List<DataCases>();
+            string path2 = @"F:\Interview\FAO_Tasks\InputData\data_cases_2.csv";
 
-            string[] rows;
-            string[] columns;
+            Console.WriteLine("Enter your needed indicator: ");
+            int indicators = int.Parse(Console.ReadLine());
 
-            foreach (var path in paths) 
+            switch (indicators)
             {
-                rows = File.ReadAllLines(path);
-                
-                for (int i = 1; i < rows.Length; i++)
-                {
-                    DataCases dc = new DataCases();
+                case 1:
+                    Console.WriteLine("First Indicators value are ready.");
+                    FirstIndicators firstIndicators = new FirstIndicators();
+                    firstIndicators.FirstIndicator(path1);
+                    break;
 
-                    columns = rows[i].Split(',');
-                    dc.uuid = Encoding.Unicode.GetBytes(columns[0]);
-                    dc.datetime = Convert.ToDateTime(columns[1]);
-                    dc.species = columns[2];
-                    dc.number_morbidity = Convert.ToInt32(columns[3]);
-                    dc.disease_id = Convert.ToInt32(columns[4]);
-                    dc.number_mortality = Convert.ToInt32(columns[5]);
-                    dc.total_number_cases = Convert.ToInt32(columns[6]);
-                    dc.location = columns[7];
+                default:
+                    Console.WriteLine(String.Format("Unknown indicators: {0}", indicators));
 
-                    dataCases.Add(dc);
-                }
+                    break;
             }
-
-            Dictionary<string, List<DataCases>> casesDictionary = dataCases.GroupBy(x => x.location).ToDictionary(x => x.Key, x => x.ToList());
-      
-            foreach (var item in casesDictionary)
-            {
-                Console.WriteLine("Number of Locations: " + item.Key+" : "+item.Value.Count());
-
-            }
-
+            
         }
-    }
-
-    public class DataCases
-    {
-        public byte[] uuid { get; set; }
-        public DateTime datetime { get; set; }
-        public string species { get; set; }
-        public int number_morbidity { get; set; }
-        public int disease_id { get; set; }
-        public int number_mortality { get; set; }
-        public int total_number_cases { get; set; }
-        public string location { get; set; }
-    }
-
-    public class Disease
-    {
-        public int id { get; set; }
-        public String name { get; set; }
     }
 }
